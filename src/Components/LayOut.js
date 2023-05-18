760abbd5e4b34d8c051cc0d1f4ee3c7061a98306
Kunal Chopra <kunal@kdbase.com>
2023-05-17 22:35:41 -0400

    made navbar mobile friendly

--- src/Components/LayOut.js
+++ src/Components/LayOut.js
@@ -4,16 +4,19 @@ import Navbar from 'react-bootstrap/Navbar'
 function  LayOut(){
  return (
     <>
-        <Navbar variant="dark" className = "justify-content-center">
-            <Navbar.Brand href="/">
-                <img src="images/header.png" width="100px" height="40px" alt=""/>
+        <Navbar variant="dark" collapseOnSelect expand="lg">
+            <Navbar.Brand href="/" className="ms-4">
+                <img src="images/header.png" height="40px" alt=""/>
             </Navbar.Brand>
-            <Nav className="nav-pills" bg="dark">
-                <NavLink className="nav-link" to="/statistics">Statistics</NavLink>
-                <NavLink className="nav-link" to="https://discordbots.org/bot/414925323197612032">Bot Page</NavLink>
-                <NavLink className="nav-link" to="https://discord.gg/xMh2YTb">Support Server</NavLink>
-                <NavLink className="nav-link" to="/premium">Premium</NavLink>
-            </Nav>
+            <Navbar.Toggle/>
+            <Navbar.Collapse className = "justify-content-center" width="100%">
+                <Nav className="nav-pills" bg="dark">
+                    <NavLink className="nav-link" to="/statistics">Statistics</NavLink>
+                    <NavLink className="nav-link" to="https://discordbots.org/bot/414925323197612032">Bot Page</NavLink>
+                    <NavLink className="nav-link" to="https://discord.gg/xMh2YTb">Support Server</NavLink>
+                    <NavLink className="nav-link" to="/premium">Premium</NavLink>
+                </Nav>
+            </Navbar.Collapse>
         </Navbar >
         <Outlet/>
         <div className = "footer bg-dark text-left">
