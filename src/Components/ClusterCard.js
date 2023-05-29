c8a2af61892ce197b724fb5e7dc922b4500ea109
Kunal Chopra <kunal@kdbase.com>
2023-05-29 19:29:24 -0400

    fixed buttons of heatmap

--- src/Components/ClusterCard.js
+++ src/Components/ClusterCard.js
@@ -1,30 +1,25 @@
 import Cluster from './Cluster'
-
+import Card from 'react-bootstrap/Card'
+import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup'
+import ToggleButton from 'react-bootstrap/ToggleButton'
 function  ClusterCard(){
  return (
-	<div class="card text-light bg-dark ">
-		<div class="card-header text-center">
-			<button class="btn bg-dark text-white minimize-btn" data-toggle="collapse" data-target="#clusters" aria-expanded="true" aria-controls="collapse"><div class ="rectangle"></div></button>
+	<Card className="text-light bg-dark ">
+		<div className="card-header text-center">
+			<button className="btn bg-dark text-white minimize-btn" data-toggle="collapse" data-target="#clusters" aria-expanded="true" aria-controls="collapse"><div class ="rectangle"></div></button>
 			Clusters							
 		</div>
-		<div class="card-body collapse show" id="clusters">
+		<Card.Body className="collapse show" id="clusters">
 			<p>Per-shard status of clusters. Hover over each shard for a preview of the shards' status. Click on a shard for more information.</p>
-			Heatmap type:&nbsp;&nbsp;&nbsp;&nbsp;<div class="btn-group btn-group-toggle" data-toggle="buttons">
-				<label class="btn btn-dark active">
-					<input type="radio" name="options" id="online" autocomplete="off" checked/>Online
-				</label>
-				<label class="btn btn-dark">
-					<input type="radio" name="options" id="voice_clients" autocomplete="off"/>Voice Clients
-				</label>
-				<label class="btn btn-dark">
-					<input type="radio" name="options" id="latency" autocomplete="off"/>Latency
-				</label>
-				<label class="btn btn-dark">
-					<input type="radio" name="options" id="memory" autocomplete="off"/>Memory usage
-					</label>
-				</div>
-		</div>
-	</div>
+			Heatmap type:&nbsp;&nbsp;&nbsp;&nbsp;
+				<ToggleButtonGroup type = "radio" name="heatmap" defaultValue={1} className="" >
+					<ToggleButton  name="options" id="online" className="btn-dark" value={1}>Online  </ToggleButton>
+					<ToggleButton  name="options" id="voice_clients" className="btn-dark" value={2}>Voice Clients</ToggleButton>
+					<ToggleButton  name="options" id="latency" className="btn-dark" value={3}>Latency</ToggleButton>
+					<ToggleButton  name="options" id="memory" className="btn-dark" value={4}>Memory usage</ToggleButton>
+				</ToggleButtonGroup>
+		</Card.Body>
+	</Card>
     
 )}
 export default ClusterCard
