0d345a72882638cda52776bec613a90f2531e5c6
Kunal Chopra <kunal@kdbase.com>
2023-06-05 19:16:04 -0400

    Fixed cluster card and hover color of the heatmap

--- src/Components/ClusterCard.js
+++ src/Components/ClusterCard.js
@@ -1,24 +1,30 @@
+import { useState } from 'react'
 import Cluster from './Cluster'
 import Card from 'react-bootstrap/Card'
 import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup'
 import ToggleButton from 'react-bootstrap/ToggleButton'
+import Button from 'react-bootstrap/Button'
+import Collapse from 'react-bootstrap/Collapse'
 function  ClusterCard(){
+	const [open, setOpen] = useState(true)
  return (
 	<Card className="text-light bg-dark ">
-		<div className="card-header text-center">
-			<button className="btn bg-dark text-white minimize-btn" data-toggle="collapse" data-target="#clusters" aria-expanded="true" aria-controls="collapse"><div class ="rectangle"></div></button>
-			Clusters							
-		</div>
-		<Card.Body className="collapse show" id="clusters">
-			<p>Per-shard status of clusters. Hover over each shard for a preview of the shards' status. Click on a shard for more information.</p>
-			Heatmap type:&nbsp;&nbsp;&nbsp;&nbsp;
-				<ToggleButtonGroup type = "radio" name="heatmap" defaultValue={1} className="" >
-					<ToggleButton  name="options" id="online" className="btn-dark" value={1}>Online  </ToggleButton>
-					<ToggleButton  name="options" id="voice_clients" className="btn-dark" value={2}>Voice Clients</ToggleButton>
-					<ToggleButton  name="options" id="latency" className="btn-dark" value={3}>Latency</ToggleButton>
-					<ToggleButton  name="options" id="memory" className="btn-dark" value={4}>Memory usage</ToggleButton>
-				</ToggleButtonGroup>
-		</Card.Body>
+		<Card.Header className="text-center">
+			<Button variant = "dark" onClick={()=>setOpen(!open)} className="bg-dark text-white minimize-btn" aria-expanded={open} aria-controls="collapse"><div className ="rectangle"></div></Button>
+			Clusters
+		</Card.Header>
+		<Collapse in={open}>
+			<Card.Body>
+				<p>Per-shard status of clusters. Hover over each shard for a preview of the shards' status. Click on a shard for more information.</p>
+				Heatmap type:&nbsp;&nbsp;&nbsp;&nbsp;
+					<ToggleButtonGroup type = "radio" name="heatmap" defaultValue={1}>
+						<ToggleButton variant = "dark" id="online" value={1}>Online  </ToggleButton>
+						<ToggleButton variant = "dark" id="voice_clients" value={2}>Voice Clients</ToggleButton>
+						<ToggleButton variant = "dark" id="latency" value={3}>Latency</ToggleButton>
+						<ToggleButton variant = "dark" id="memory" value={4}>Memory usage</ToggleButton>
+					</ToggleButtonGroup>
+			</Card.Body>
+		</Collapse>
 	</Card>
     
 )}
