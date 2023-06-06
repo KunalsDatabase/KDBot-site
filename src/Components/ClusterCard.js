242a7e8d7827a715fec5d223ba774b2e73d39dee
Kunal Chopra <kunal@kdbase.com>
2023-06-06 15:12:22 -0400

    Deleted Middle man component

--- src/Components/ClusterCard.js
+++ src/Components/ClusterCard.js
@@ -8,24 +8,26 @@ import Collapse from 'react-bootstrap/Collapse'
 function  ClusterCard(){
 	const [open, setOpen] = useState(true)
  return (
-	<Card className="text-light bg-dark ">
-		<Card.Header className="text-center">
-			<Button variant = "dark" onClick={()=>setOpen(!open)} className="bg-dark text-white minimize-btn" aria-expanded={open} aria-controls="collapse"><div className ="rectangle"></div></Button>
-			Clusters
-		</Card.Header>
-		<Collapse in={open}>
-			<Card.Body>
-				<p>Per-shard status of clusters. Hover over each shard for a preview of the shards' status. Click on a shard for more information.</p>
-				Heatmap type:&nbsp;&nbsp;&nbsp;&nbsp;
-					<ToggleButtonGroup type = "radio" name="heatmap" defaultValue={1}>
-						<ToggleButton variant = "dark" id="online" value={1}>Online  </ToggleButton>
-						<ToggleButton variant = "dark" id="voice_clients" value={2}>Voice Clients</ToggleButton>
-						<ToggleButton variant = "dark" id="latency" value={3}>Latency</ToggleButton>
-						<ToggleButton variant = "dark" id="memory" value={4}>Memory usage</ToggleButton>
-					</ToggleButtonGroup>
-			</Card.Body>
-		</Collapse>
-	</Card>
+	<div class="col px-lg-1">
+		<Card className="text-light bg-dark ">
+			<Card.Header className="text-center">
+				<Button variant = "dark" onClick={()=>setOpen(!open)} className="bg-dark text-white minimize-btn" aria-expanded={open} aria-controls="collapse"><div className ="rectangle"></div></Button>
+				Clusters
+			</Card.Header>
+			<Collapse in={open}>
+				<Card.Body>
+					<p>Per-shard status of clusters. Hover over each shard for a preview of the shards' status. Click on a shard for more information.</p>
+					Heatmap type:&nbsp;&nbsp;&nbsp;&nbsp;
+						<ToggleButtonGroup type = "radio" name="heatmap" defaultValue={1}>
+							<ToggleButton variant = "dark" id="online" value={1}>Online  </ToggleButton>
+							<ToggleButton variant = "dark" id="voice_clients" value={2}>Voice Clients</ToggleButton>
+							<ToggleButton variant = "dark" id="latency" value={3}>Latency</ToggleButton>
+							<ToggleButton variant = "dark" id="memory" value={4}>Memory usage</ToggleButton>
+						</ToggleButtonGroup>
+				</Card.Body>
+			</Collapse>
+		</Card>
+	</div>
     
 )}
 export default ClusterCard
