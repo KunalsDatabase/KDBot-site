0911d4f62e4ed52c7ba99f0edcbb2c1fd863af22
Kunal Chopra <kunal@kdbase.com>
2023-06-05 23:00:45 -0400

    memory card

--- src/Components/MemoryCard.js
+++ src/Components/MemoryCard.js
@@ -1,8 +1,24 @@
+import Collapse from 'react-bootstrap/Collapse'
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
+
+import { useState } from 'react'
+
 function  MemoryCard(){
+	const [open, setOpen] = useState(true)
  return (
-	<>
-
-
-	</>
+	<div className= "col-12 px-lg-1">
+		<Card className="text-light bg-dark">
+			<Card.Header className="text-center">
+			<Button variant = "dark" onClick={()=>setOpen(!open)} className="bg-dark text-white minimize-btn" aria-expanded={open} aria-controls="collapse"><div className ="rectangle"></div></Button>
+				Live memory usage
+			</Card.Header>
+			<Collapse in={open}>
+				<Card.Body>
+					<canvas id="lineChart"></canvas>
+				</Card.Body>
+			</Collapse>
+		</Card>
+	</div>
 )}
 export default MemoryCard
