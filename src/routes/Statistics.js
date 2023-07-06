69de3b64658859becb05f640bda576d90862fbe4
Kunal Chopra <kunal@kdbase.com>
2023-07-05 20:41:35 -0400

    Switched api call logic to async/await

--- src/routes/Statistics.js
+++ src/routes/Statistics.js
@@ -13,43 +13,40 @@ function  Statistics(){
     const [memory,setMemory] = useState({})
     const [globalMemory,setGlobalMemory] = useState([])
     const [aggregates,setAggregates] = useState([])
-    useEffect(
-        ()=>{
-            function getData(){
-                fetch("http://192.168.0.62/kdbot/botapi.php?botinfo")
-                .then(res=>res.json())
-                .then(stats=>{
-                    setAggregates(StatsPreprocess(stats))
-                    setStats(stats)
-                })
-                fetch("http://192.168.0.62/kdbot/botapi.php?memory")
-                .then(res=>res.json())
-                .then(memoryData=>{
-                    setMemory(memoryData)
-                    setGlobalMemory(MemoryPreprocess(memoryData))
-                })               
+    useEffect(()=>{
+            async function getData(){
+                let stats = await fetch("http://192.168.0.62/kdbot/botapi.php?botinfo")
+                let memoryData = await fetch("http://192.168.0.62/kdbot/botapi.php?memory")
+                stats= await stats.json()
+                memoryData= await memoryData.json()
+                setAggregates(StatsPreprocess(stats))
+                setStats(stats)
+                setMemory(memoryData)
+                setGlobalMemory(MemoryPreprocess(memoryData))       
             }
             getData()
-            const APIinterval=setInterval(getData
-            ,5000)
+            const APIinterval=setInterval(getData,5000)
             return ()=>clearInterval(APIinterval)
         },[])
  return (
     <Container fluid>
+
         <Row className = "mt-2 row-eq-height">
 			<StatCard Heading="Guilds" Value = {aggregates.guilds}/>
 			<StatCard Heading="Voice Clients" Value = {aggregates.voice}/>
 			<StatCard Heading="Average Latency" Value = {aggregates.latency}/>
 			<StatCard Heading="# of DB entries" Value = {aggregates.dbnum}/>
         </Row>
+
         <Row className = "mt-2 row-eq-height">
         <StatCard Heading="Shards" Value ={aggregates.shards}/>
 			<StatCard Heading="Uptime" Value = {aggregates.uptime}/>
 			<StatCard Heading="Memory Usage" Value = {aggregates.memusage}/>
 			<StatCard Heading="Characters used" Value = {aggregates.numchars}/>
         </Row>
+
         <Row className = "mt-2 row-eq-height">
-            <ClusterCard stats = {stats}/>
+            <ClusterCard stats = {stats} memory={memory}/>
         </Row>
 
         <Row className = "mt-2 row-eq-height">
@@ -60,6 +57,7 @@ function  Statistics(){
             <DonateCard/>
             <UsageCard/>
         </Row>
+
      </Container>
  )
 }
