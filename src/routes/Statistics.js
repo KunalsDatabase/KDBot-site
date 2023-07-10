b2a8346b5f939fcc407b16effd86b2d6821a6713
Kunal Chopra <kunal@kdbase.com>
2023-07-10 14:34:51 -0400

    error pop-up handling for cluster

--- src/routes/Statistics.js
+++ src/routes/Statistics.js
@@ -8,21 +8,41 @@ import UsageCard from '../Components/UsageCard'
 import Row from 'react-bootstrap/Row'
 import StatsPreprocess from '../Utils/StatsPreprocess'
 import MemoryPreprocess from '../Utils/MemoryPreprocess'
+import Alert from 'react-bootstrap/Alert'
 function  Statistics(){
     const [stats,setStats] = useState({})
     const [memory,setMemory] = useState({})
     const [globalMemory,setGlobalMemory] = useState([])
     const [aggregates,setAggregates] = useState([])
+    const [errorMessage,setErrorMessage] = useState("")
+    const [errorShow,setErrorShow] = useState(false)
+    function handleError(e){
+        setErrorMessage((prevState)=>{
+            if(prevState.message === e.message) return prevState //Prevent duplicate errors
+            setErrorShow(true)
+            return e
+        })
+    }
     useEffect(()=>{
             async function getData(){
-                let stats = await fetch("http://192.168.0.62/kdbot/botapi.php?botinfo")
-                let memoryData = await fetch("http://192.168.0.62/kdbot/botapi.php?memory")
-                stats= await stats.json()
-                memoryData= await memoryData.json()
-                setAggregates(StatsPreprocess(stats))
-                setStats(stats)
-                setMemory(memoryData)
-                setGlobalMemory(MemoryPreprocess(memoryData))       
+                try{
+                    let [statsResponse,memoryResponse] = await Promise.all([fetch("http://kdbase.com/kdbot/botapi.php?botinfo"),fetch("http://kdbase.com/kdbot/botapi.php?memory")])
+                    let [stats,memoryData] = await Promise.all([statsResponse.json(),memoryResponse.json()])
+                    Object.entries(stats).forEach(function([key, value]) {
+                        if(Object.keys(stats[key]).length === 0){
+                            delete stats[key]
+                            handleError({heading:"Cluster Error",message:"The cluster '"+key+"' is currently offline or in the process of restarting."})
+                        }
+                    })
+                    setMemory(memoryData)
+                    setGlobalMemory(MemoryPreprocess(memoryData))
+                    setAggregates(StatsPreprocess(stats))
+                    setStats(stats)
+
+                }
+                catch(e){
+                    console.log("Error while fetching data:",e)
+                }
             }
             getData()
             const APIinterval=setInterval(getData,5000)
@@ -30,7 +50,12 @@ function  Statistics(){
         },[])
  return (
     <Container fluid>
-
+      {errorShow && <Alert variant="danger" onClose={() => setErrorShow(false)} dismissible>
+        <Alert.Heading>{errorMessage.heading}</Alert.Heading>
+        <p>
+            {errorMessage.message}
+        </p>
+      </Alert>}
         <Row className = "mt-2 row-eq-height">
 			<StatCard Heading="Guilds" Value = {aggregates.guilds}/>
 			<StatCard Heading="Voice Clients" Value = {aggregates.voice}/>
@@ -39,10 +64,10 @@ function  Statistics(){
         </Row>
 
         <Row className = "mt-2 row-eq-height">
-        <StatCard Heading="Shards" Value ={aggregates.shards}/>
-			<StatCard Heading="Uptime" Value = {aggregates.uptime}/>
-			<StatCard Heading="Memory Usage" Value = {aggregates.memusage}/>
-			<StatCard Heading="Characters used" Value = {aggregates.numchars}/>
+            <StatCard Heading="Shards" Value ={aggregates.shards}/>
+            <StatCard Heading="Uptime" Value = {aggregates.uptime}/>
+            <StatCard Heading="Memory Usage" Value = {aggregates.memusage}/>
+            <StatCard Heading="Characters used" Value = {aggregates.numchars}/>
         </Row>
 
         <Row className = "mt-2 row-eq-height">
