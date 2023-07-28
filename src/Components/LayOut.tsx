2683589034e8918fc23a0b2f454caf660109ae3e
Kunal Chopra <kunal@kdbase.com>
2023-07-27 21:24:05 -0400

    navbar improvements

--- src/Components/LayOut.tsx
+++ src/Components/LayOut.tsx
@@ -1,20 +1,23 @@
 import { NavLink, Outlet } from "react-router-dom"
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
+import NavDropdown from 'react-bootstrap/NavDropdown'
 function  LayOut(){
  return (
     <>
         <Navbar variant="dark" collapseOnSelect expand="lg">
             <Navbar.Brand href="/" className="ms-4">
-                <img src="images/header.png" height="40px" alt=""/>
+                <img src="/images/header.png" height="40px" alt=""/>
             </Navbar.Brand>
             <Navbar.Toggle/>
             <Navbar.Collapse className = "justify-content-center">
                 <Nav className="nav-pills">
                     <NavLink className="nav-link" to="statistics">Statistics</NavLink>
-                    <NavLink className="nav-link" to="https://discordbots.org/bot/414925323197612032">Bot Page</NavLink>
-                    <NavLink className="nav-link" to="https://discord.gg/xMh2YTb">Support Server</NavLink>
                     <NavLink className="nav-link" to="premium">Premium</NavLink>
+                    <NavDropdown title="External Links" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="https://discordbots.org/bot/414925323197612032" rel="noreferrer">⧉ Bot Page</NavDropdown.Item>
+                        <NavDropdown.Item href="https://discord.gg/xMh2YTb" rel="noreferrer">⧉ Support Server</NavDropdown.Item>
+                    </NavDropdown>
                 </Nav>
             </Navbar.Collapse>
         </Navbar >
