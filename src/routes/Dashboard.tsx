30024c8357ae04ce35d9adcc7b1274cd50ee8419
Kunal Chopra <kunal@kdbase.com>
2023-10-17 23:47:21 -0400

    added tabs

--- src/routes/Dashboard.tsx
+++ src/routes/Dashboard.tsx
@@ -3,10 +3,33 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
 function  Dashboard(){
     return (
         <Container fluid>
             <h1 className = "text-light text-center">   Coming soon  </h1>
+            <Card text = "light" className="text-center bg-dark">
+        <Card.Body>
+            <Tabs
+                defaultActiveKey="servers"
+                id="dashboard-tabs"
+                className="mb-3"
+                justify
+                variant="tabs"
+                >
+                <Tab eventKey="servers" title="Servers">
+                    Server list
+                </Tab>
+                <Tab eventKey="settings" title="Settings">
+                    Bot settings
+                </Tab>
+                <Tab eventKey="subscription" title="Subscription">
+                    Subscription management
+                </Tab>
+            </Tabs>
+      </Card.Body>
+    </Card>
         </Container>
 
     )
