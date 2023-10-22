b44f24f3127dae55661a3f2b611ba50ccca6212c
Kunal Chopra <kunal@kdbase.com>
2023-10-21 22:13:03 -0400

    styling

--- src/Components/LayOut.tsx
+++ src/Components/LayOut.tsx
@@ -2,6 +2,7 @@ import { NavLink, Outlet } from "react-router-dom"
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import NavDropdown from 'react-bootstrap/NavDropdown'
+import Button from 'react-bootstrap/Button'
 function  LayOut(){
  return (
     <>
@@ -10,8 +11,8 @@ function  LayOut(){
                 <img src="/images/header.png" height="40px" alt=""/>
             </Navbar.Brand>
             <Navbar.Toggle/>
-            <Navbar.Collapse className = "justify-content-center">
-                <Nav className="nav-pills">
+            <Navbar.Collapse>
+                <Nav className="nav-pills me-auto">
                     <NavLink className="nav-link" to="statistics">Statistics</NavLink>
                     <NavLink className="nav-link" to="dashboard">Dashboard</NavLink>
                     <NavLink className="nav-link" to="premium">Premium</NavLink>
@@ -20,9 +21,14 @@ function  LayOut(){
                         <NavDropdown.Item href="https://discord.gg/xMh2YTb" rel="noreferrer">⧉ Support Server</NavDropdown.Item>
                     </NavDropdown>
                 </Nav>
+ 
+            <Button variant="primary" href="oauthlinkhere" className="me-3">Login with Discord</Button>
+
             </Navbar.Collapse>
-        </Navbar >
+        </Navbar>
+        <div className="content">
         <Outlet/>
+        </div>
         <div className = "footer bg-dark text-left">
             <span className="text-muted">Copyright ©️{new Date().getFullYear()} Kunal's Database</span>
 	    </div>
